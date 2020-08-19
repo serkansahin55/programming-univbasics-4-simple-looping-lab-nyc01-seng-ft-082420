0e1a45a051087fdfdc581eab5817dd9649f9615a
Serkan Sahin <ssahin55@outlook.com>
2020-08-19 14:18:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,1 @@
-def loop_message_five_times(message)
-  count = 0 
-  while count < 5 do
-    puts message
-    count += 1 
-  end
-end
-  
-  
-  
-  
-def loop_message_n_times(message, integer)
-  
+def loop_message_five_times
