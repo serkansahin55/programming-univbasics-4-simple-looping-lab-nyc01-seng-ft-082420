885f6d7f1073671993f76c49f25937d4fa1571e5
Serkan Sahin <ssahin55@outlook.com>
2020-08-19 14:23:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,14 @@
 def loop_message_five_times(message)
   count = 0
-  while count < 5
-  message = "Hello World."
-  puts message
-  count += 1 
-end
+  while count < 5 do
+    message = "Hello World."
+    puts message
+    count += 1 
+  end
 end
 
 
 
 def loop_message_n_timers(message, integer)
+  count = 
   
